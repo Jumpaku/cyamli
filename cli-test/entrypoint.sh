3e7cc12a71ec6ec71395fa2abcfff3f249395220
Jumpaku <Jumpaku@users.noreply.github.com>
2024-08-04 01:46:02 +0900

    test(cli-test): revise script

--- cli-test/entrypoint.sh
+++ cli-test/entrypoint.sh
@@ -5,7 +5,8 @@ for TESTCASE in */testcases.sh; do
   cd "${WORKDIR}" || exit 1
   GROUP=$(dirname "${TESTCASE}")
   cd "${GROUP}" || exit 1
-  if sh testcases.sh | grep '^NG'; then
+  if sh testcases.sh | tee test.log | grep -E '^NG'; then
+    cat test.log
     exit 1
   fi
 done
