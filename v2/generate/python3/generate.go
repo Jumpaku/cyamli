ccac67a55f083237d16c9f64dfd2f38da79277c2
Jumpaku <Jumpaku@users.noreply.github.com>
2025-05-30 03:05:14 +0900

    feat(v2/python3): implement python3 code generator

---
+++ v2/generate/python3/generate.go
@@ -0,0 +1,28 @@
+package python3
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"github.com/Jumpaku/cyamli/v2/schema"
+	"io"
+	"text/template"
+)
+
+//go:embed cli_gen.py.tpl
+var cliGenPyTemplate string
+var executor = template.Must(template.New("cli_gen.py.tpl").Parse(cliGenPyTemplate))
+
+func Generate(schema schema.Schema, packageName, generator string, out io.Writer) error {
+	d := ConstructData(schema, packageName, generator)
+
+	buf := bytes.NewBuffer(nil)
+	if err := executor.Execute(buf, d); err != nil {
+		return fmt.Errorf("fail to execute template: %w", err)
+	}
+
+	if _, err := out.Write(buf.Bytes()); err != nil {
+		return fmt.Errorf("fail to write generated code: %w", err)
+	}
+	return nil
+}
