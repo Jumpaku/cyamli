b66cdbc416d048b8e67b4ea9a754375859950937
Jumpaku <Jumpaku@users.noreply.github.com>
2025-07-08 01:46:55 +0900

    test(v2): refactor test generator for Kotlin

--- v2/generate/kotlin/generate.go
+++ v2/generate/kotlin/generate.go
@@ -27,3 +27,22 @@ func Generate(schema schema.Schema, packageName, generator string, out io.Writer
 	}
 	return nil
 }
+
+//go:embed cli_test.gen.kt.tpl
+var cliTestGenKtTemplate string
+var executorTest = template.Must(template.New("cli_test.gen.kt.tpl").Parse(cliTestGenKtTemplate))
+
+func GenerateTest(schema schema.Schema, packageName, generator string, out io.Writer) error {
+	d := ConstructData(schema, packageName, generator)
+
+	buf := bytes.NewBuffer(nil)
+	if err := executorTest.Execute(buf, d); err != nil {
+		return fmt.Errorf("fail to execute template: %w", err)
+	}
+
+	// Write the generated code without any post-processing
+	if _, err := out.Write(buf.Bytes()); err != nil {
+		return fmt.Errorf("fail to write generated code: %w", err)
+	}
+	return nil
+}
